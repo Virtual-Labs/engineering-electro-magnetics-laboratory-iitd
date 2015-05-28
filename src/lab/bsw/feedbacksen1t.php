31c6e4733a598c11c4c91edbec56d3ef45d04bc0
sumitdahiyavlab <sumitdahiya.iitd@gmail.com>
2015-05-28 20:16:22 +0530

    add new src folder

---
+++ src/lab/bsw/feedbacksen1t.php
@@ -0,0 +1,98 @@
+<?php
+if(!empty($_POST)) extract($_POST);
+if(!empty($_GET)) extract($_GET);
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Thanks</title>
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+
+<style type="text/css">
+body { font-family: verdana, helvetica, sans-serif, arial; font-size: 80%; background-color: #fffffd; }
+h1 { font-family: helvetica, sans-serif, arial; color: #9F141A; font-size: 150%; margin-bottom: 0.2em; }}
+</style>
+
+</head>
+
+<body>
+
+<?php
+
+$headers = "From: $email";
+$message .= "
+
+Virtual Engineering Electromagnetics Laboratory Feedback send to you from $email
+___________________________________________________________________________________
+
+$text
+";
+
+$message2 .= "
+
+Thank you for your email message regarding the subject: $sub.\n
+I will respond to it as quickly as I can. Please feel free to write again!\n\n
+Thanking you,\n
+
+
+abegaonkar.mahesh@gmail.com 
+http://web.iitd.ac.in/~virtual\n
+\n\n
+Your original message was
+$text
+";
+$headers2 .= "From: abegaonkar.mahesh@gmail.com\n";
+$sub2.="Feedback Verification: $sub";
+$sub3 .="[Online counselling] $sub";
+
+?> 
+
+
+<h1>Thank you!</h1><p>Your message has been sent. You gave your email address as<p>
+<nobr><span style="color:red;font-size:150%;font-weight:bold;"><?php print $email; ?></span></nobr>
+
+
+<p>If not correct, please
+<script language='javascript' type='text/javascript'>
+document.write('<a href="javascript:history.go(-1);">go back</a>');
+</script>
+<noscript>go back</noscript>
+and send again.
+<p>
+
+<!-- CHANGE THE WORDING BELOW TO THE NAME OF YOUR SITE -->
+
+<script language='javascript' type='text/javascript'>
+document.write('<li><a href="http://web.iitd.ac.in/~virtual/">Return Back to Virtual Engineering Electromagnetics Laboratory, IIT Delhi</a>');
+</script>
+
+<script language='javascript' type='text/javascript'>
+setTimeout('history.go(-2)', 9000);
+</script>
+
+
+<noscript>
+<li>Click your 'back button' twice to return to main page
+</noscript> 
+
+
+<?php 
+$message = stripslashes($message);
+$message2 = stripslashes($message2); 
+$message = strip_tags ($message);
+$message2 = strip_tags ($message2);
+// INSERT EMAIL ADDRESS to whon you want to sent message BELOW
+mail("abegaonkar.mahesh@gmail.com", $sub3, $message, $headers);
+
+// donot change this line
+mail($email, $sub2, $message2, $headers2);
+?> 
+
+</body> 
+</html> 
+
+
+
---
+++ src/lab/progressbar.js
@@ -0,0 +1,85 @@
+
+n=12 // SPECIFY DURATION");
+var _progressEnd = 10;
+var _progressAt = 0;
+
+
+// Create and display the progress dialog.
+// end: The number of steps to completion
+function ProgressCreate(end) {
+	// Initialize state variables
+	_progressEnd = end;
+	_progressAt = 0;
+
+	// Move layer to center of window to show
+	if (document.all) {	// Internet Explorer
+		progress.className = 'show';
+		progress.style.left = (document.body.clientWidth/2) - (progress.offsetWidth/2);
+		progress.style.top = document.body.scrollTop+(document.body.clientHeight/2) - (progress.offsetHeight/2);
+	} else if (document.layers) {	// Netscape
+		document.progress.visibility = true;
+		document.progress.left = (window.innerWidth/2) - 100;
+		document.progress.top = pageYOffset+(window.innerHeight/2) - 40;
+	} else if (document.getElementById) {	// Netscape 6+
+		document.getElementById("progress").className = 'show';
+		document.getElementById("progress").style.left = (window.innerWidth/2)- 100;
+		document.getElementById("progress").style.top = pageYOffset+(window.innerHeight/2) - 40;
+	}
+
+	ProgressUpdate();	// Initialize bar
+}
+
+// Hide the progress layer
+function ProgressDestroy() {
+	// Move off screen to hide
+	if (document.all) {	// Internet Explorer
+		progress.className = 'hide';
+	} else if (document.layers) {	// Netscape
+		document.progress.visibility = false;
+	} else if (document.getElementById) {	// Netscape 6+
+		document.getElementById("progress").className = 'hide';
+	}
+}
+
+// Increment the progress dialog one step
+function ProgressStepIt() {
+	_progressAt++;
+	if(_progressAt > _progressEnd) _progressAt = _progressAt % _progressEnd;
+	ProgressUpdate();
+}
+
+// Update the progress dialog with the current state
+function ProgressUpdate() {
+	var n = (_progressWidth / _progressEnd) * _progressAt;
+	if (document.all) {	// Internet Explorer
+		var bar = dialog.bar;
+ 	} else if (document.layers) {	// Netscape
+		var bar = document.layers["progress"].document.forms["dialog"].bar;
+		n = n * 0.55;	// characters are larger
+	} else if (document.getElementById){
+                var bar=document.dialog.bar
+        }
+	var temp = _progressBar.substring(0, n);
+	bar.value = temp;
+}
+
+// Demonstrate a use of the progress dialog.
+function Demo() {
+	ProgressCreate(10);
+	window.setTimeout("Click()", 100);
+}
+
+function Click() {
+	if(_progressAt >= _progressEnd) {
+		ProgressDestroy();
+		return;
+	}
+	ProgressStepIt();
+	window.setTimeout("Click()", (duration-1)*1000/10);
+}
+
+function CallJS(jsStr) { //v2.0
+  return eval(jsStr)
+}
+
+
